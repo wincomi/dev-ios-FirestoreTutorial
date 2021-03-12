6306e1a5da99e9965a0fff1273457499f179c275
COMI <admin@wincomi.com>
2021-03-12 16:42:15 +0900

    Initial Commit

---
+++ FirestoreTutorial/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  FirestoreTutorial
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+}
